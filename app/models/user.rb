1bda40343e98484e12a36a4d2d8eb6480f2975f8
Davide Ghezzi <davideghz@gmail.com>
2016-01-02 17:03:47 +0100

    make user model and include secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+
+  # this is a callback function. See more at http://api.rubyonrails.org/v4.2.0/classes/ActiveRecord/Callbacks.html
+  before_save { self.email = self.email.downcase }
+
   validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false}
+  validates :password, length: { minimum: 6 }
+  has_secure_password
 end
