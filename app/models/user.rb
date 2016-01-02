7bd2524e99c87e7904e56178094c9e22c3988ab4
Davide Ghezzi <davideghz@gmail.com>
2016-01-02 20:17:36 +0100

    add uppercase test and enforce email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
 
   # this is a callback function. See more at http://api.rubyonrails.org/v4.2.0/classes/ActiveRecord/Callbacks.html
-  before_save { self.email = self.email.downcase }
+  before_save { self.email.downcase! }
 
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false}
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -59,4 +59,11 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "email should be saved as lowercase" do
+    mixed_case_email = "test_UPPERCASE@example.org"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 end
