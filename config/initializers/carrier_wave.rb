06662da7f6cc21648cf9845ab867ce287eb58b36
Davide Ghezzi <davideghz@gmail.com>
2016-01-27 17:44:12 +0100

    playing with fog config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.root = Rails.root.join('tmp') # adding these...
-    config.cache_dir = 'carrierwave' # ...two lines
+    # config.root = Rails.root.join('tmp') # adding these...
+    # config.cache_dir = 'carrierwave' # ...two lines
 
     config.fog_credentials = {
         # Configuration for Amazon S3
@@ -11,7 +11,7 @@ if Rails.env.production?
         :region                => ENV['S3_REGION']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
-    config.fog_public     = false                                   # optional, defaults to true
-    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+    # config.fog_public     = false                                   # optional, defaults to true
+    # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
 end
